bbf35455be9f3727e479be77bc204704a80acf12
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-29 20:40:44 +0530

    Success : Validate Email

--- EmailValidateRegex.java
+++ EmailValidateRegex.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class LastName {
+public class EmailValidateRegex {
 	public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter first name");
